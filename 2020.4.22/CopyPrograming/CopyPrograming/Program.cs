28510cba7cad95b5479c810b913a19f4a68a48c5
yungjin702 <yungjin702@gmail.com>
2020-11-21 23:00:38 +0900

    2020.4.22

---
+++ 2020.4.22/CopyPrograming/CopyPrograming/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyPrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current OS: {0}", Environment.OSVersion);
+            Console.WriteLine($"Current OS: {Environment.OSVersion}");
+
+            Console.WriteLine($"Current OS: {Environment.CurrentDirectory}");
+            string[] drives = Environment.GetLogicalDrives();
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                Console.WriteLine($"Drive {i} : {drives[i]}");
+            }
+
+            Console.WriteLine($"Current Version of .NET : {Environment.Version}");
+        }
+    }
+}
---
+++ 2020.4.22/Homework/Homework/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Product
+    {
+        private int code;
+        private string name;
+        private int price;
+
+        static void Main(string[] args)
+        {
+            Product koreanSnack = new Product(123, "약과", 1800);
+            Product riceCake = new Product(123, "찹쌀떡", 1600);
+
+            koreanSnack.ShowInformation();
+            riceCake.ShowInformation();
+
+            Console.WriteLine("Made by 2407");
+        }
+
+        public Product()
+        {
+            code = 0;
+            name = "Null";
+            price = 0;
+        }
+
+        public Product(int c, string n, int p)
+        {
+            code = c;
+            name = n;
+            price = p;
+        }
+
+        public int GetTax()
+        {
+            int tax = (int)(price * 0.08);
+            return tax;
+        }
+
+        public int GetPriceIncludingTax()
+        {
+            return price + GetTax();
+        }
+
+        public void ShowInformation()
+        {
+            Console.WriteLine($"이름: {name}, 가격: {GetPriceIncludingTax()}");
+        }
+
+    }
+}
